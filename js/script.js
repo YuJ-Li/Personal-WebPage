225dac751d1f3041d1823ce04e3d9ee2cbf619ac
YuJ-Li <liyujinjin@hotmail.com>
2021-12-25 14:38:20 -0500

    cursor
    cursor animation added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,60 @@
+//mouse
+let mouseCursor=document.querySelector('.mouse')
+//rotation angle
+let deg=0
+//position on x axis
+let imgx=0
+//position on y axis
+let imgy=0
+//position to the left
+let imgl=0
+//position to the top
+let imgt=0
+//flip angle
+let y=0
+//counter
+let index=0
+window.addEventListener('mousemove',cursor)
+
+function cursor(xyz){
+    //x disposition after scroll
+    let scrollx=this.document.documentElement.scrollLeft
+    //y disposition after scroll
+    let scrolly=this.document.documentElement.scrollTop
+    //img to the left
+    imgx = xyz.x - mouseCursor.offsetLeft-mouseCursor.clientWidth/2 +scrollx
+    //img to the top
+    imgy = xyz.y - mouseCursor.offsetTop - mouseCursor.clientHeight/2 +scrolly
+    //rotation formula
+    deg = 360*Math.atan(imgy/imgx)/(2*Math.PI)
+    //reset index
+    index=0
+    //current x position
+    let x = event.clientX
+    //if larger than img, flip the img
+    if(mouseCursor.offsetLeft<x){
+        y=-180
+    }else{
+        y=0
+    }
+}
+
+
+setInterval(()=>{
+    //rotation and flip
+    mouseCursor.style.transform = "rotateZ("+deg+"deg) rotateY("+y+"deg)"
+    index++
+    //position and spd, stop when reached cursor
+    if(index<50){
+        imgl+=imgx/50
+        imgt+=imgy/50
+    }
+    mouseCursor.style.left = imgl+"px"
+    mouseCursor.style.top = imgt+"px"
+},10)
+
+
+// scroll
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
